ec50a52657de29ac6408553f3f0a4497bcd4d8dc
Yash Hirani <yash_h@ece.iitr.ac.in>
2021-04-15 01:11:08 +0530

    Updated first form

--- index_script.js
+++ index_script.js
@@ -1,10 +1,25 @@
 function submitCheck(form){
     console.log(form.password1.value);
     console.log(form.password2.value);
+
+    let checked = false;
+
     if(form.password1.value != form.password2.value){
         alert("Password and Confirm Password do not match.");
         return false;
     }else{
+        checked=true;
+    }
+
+    if(form.email.value.match(/[\w\d\._]+@[\w\d]+\.[\w]+/)){
+        checked=true;
+    }else{
+        alert("Invalid Email");
+        return false;
+    }
+
+    if(checked){
+        alert("Signup Successful");
         return true;
     }
 
